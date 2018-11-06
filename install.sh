94aaf426f75c41963e65040c7c15cfcdbd0c8ffa
Peter Hickman <peterhickman386@googlemail.com>
2018-11-06 21:41:53 +0000

    an install script

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+if ! [ "$(id -u)" = 0 ]; then
+  echo 'You must be root to do this.' 1>&2
+  exit 1
+fi
+
+##
+# When installing we would install as root, however on
+# BSD based systems (such as OSX) this is not a valid
+# group / user for install so we use the numeric value
+##
+ROOT="0"
+
+TARGET="/usr/local/bin/chop"
+
+NIM_FOUND=$(command -v nim)
+
+CHOICE="$1"
+
+if [ "$CHOICE" = "nim" ]; then
+  if [ -z "$NIM_FOUND" ]; then
+    echo "The nim compiler is not found, unable to install"
+    exit 1
+  fi
+
+  echo "Installing the nim version to $TARGET"
+  nim compile -d:release chop.nim
+  install -g $ROOT -o $ROOT -m 0644 chop $TARGET
+  rm chop
+elif [ "$CHOICE" = "ruby" ]; then
+  echo "Installing the ruby version to $TARGET"
+  install -g $ROOT -o $ROOT -m 0644 chop.rb $TARGET
+else
+  echo "Pass 'nim' or 'ruby' to install the appropriate version"
+  exit 1
+fi
